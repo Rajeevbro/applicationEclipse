21ceffe30fc295455df5c563e28e2f4b1af2dc10
Rajeev Chapagain <rajeevchapagain01@gmail.com>
2024-07-25 14:55:04 -0400

    Changes are staged for commiting

--- src/main/java/com/eclipseTest/applicationEclipse/repository/ProductRepository.java
+++ src/main/java/com/eclipseTest/applicationEclipse/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.eclipseTest.applicationEclipse.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eclipseTest.applicationEclipse.enity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
--- src/main/java/com/eclipseTest/applicationEclipse/service/ProductService.java
+++ src/main/java/com/eclipseTest/applicationEclipse/service/ProductService.java
@@ -1,5 +1,30 @@
 package com.eclipseTest.applicationEclipse.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eclipseTest.applicationEclipse.dto.ProductDto;
+import com.eclipseTest.applicationEclipse.enity.Product;
+import com.eclipseTest.applicationEclipse.repository.ProductRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ProductService {
+	
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
+	
+	
+	public String saveProduct(ProductDto productDto)
+	{
+		
+		
+		return "success";
+	}
 
 }
